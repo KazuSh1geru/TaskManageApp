a57e1a8be2abea3dfa6f099c784a80b01216250a
kazushi takakkusagi <lmi2022-06@la-study.com>
2022-08-08 16:05:36 +0900

    step1 done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def show
+    # あとでセッションに書き換える
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success]  = "登録しました"
+      redirect_to root_path
+    else
+      flash.now[:danger]  = "登録に失敗しました"
+      render :new
+
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(
+      :name, :birthday, :email, :password, :password_confirmation
+    )
+  end
+end
